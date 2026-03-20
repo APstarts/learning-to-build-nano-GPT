575a41426a3636499ea41fa624b5c30cfdd7eb48
Anurag Pal <tanmaypal123@outlook.com>
2026-03-21 03:22:21 +0530

    scaled up the model and added/modified the hyperparameters

--- bigram.py
+++ bigram.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32 #No. of dimensions of embeddings
+n_embd = 384 #No. of dimensions of embeddings
+n_head = 6
+n_layer = 6
+dropout = 0.2
 # ------------
 
 torch.manual_seed(1337)
@@ -67,6 +70,7 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.tril = torch.tril(torch.ones(block_size, block_size))
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B, T, C = x.shape
@@ -77,6 +81,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2,-1) * C**-0.5 #(B, T, C) @ (B, C, T) --> (B, T, T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
         wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
         #perfrom the weighted aggregation of the values
         v = self.value(x) #(B, T, C)
         out = wei @ v #(B, T, T) @ (B, T, C) --> (B, T, C)
@@ -89,10 +94,11 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
-        out = self.proj(out)
+        out = self.dropout(self.proj(out))
         return out
 
 class FeedForward(nn.Module):
@@ -103,7 +109,8 @@ class FeedForward(nn.Module):
         self.net = nn.Sequential(
             nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
-            nn.Linear(4 * n_embd, n_embd)
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout)
         )
     
     def forward(self, x):
@@ -134,14 +141,8 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd) #each vocabulary item will have a 32 dimensional embedding vector.
         self.position_embedding_table = nn.Embedding(block_size,n_embd)
-        self.blocks = nn.Sequential(
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            nn.LayerNorm(n_embd)
-        )
-        self.sa_heads = MultiHeadAttention(4, n_embd//4) #sa_head means self-attention head. 4 heads of 8-dimensional self-attention
-        self.ffwd = FeedForward(n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for  _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
         #linear layer: initializing the weights and biases. n_embd is the features as they are embedding values. so effectively it will initialize weights with shape (n_embd, vocab_size) and bias with shape ([vocab_size])
         self.lm_head = nn.Linear(n_embd,vocab_size) #lm_heads stands for language modelling head. we are using it here to go from token_emb to logits
 
@@ -151,8 +152,8 @@ class BigramLanguageModel(nn.Module):
         token_emb = self.token_embedding_table(idx) # (B,T,C) so here we are indexing and looking into the values in the embedding table for particular examples in a batch.
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) #(T,C)
         x = token_emb + pos_emb # (B,T,C)
-        x = self.sa_heads(x) # apply one head of self-attention. (B, T, C)
         x = self.blocks(x) # (B, T, C)
+        x = self.ln_f(x) # (B, T, C)
         logits = self.lm_head(x)  # (B,T,Vocab_size)
 
         if targets is None:
