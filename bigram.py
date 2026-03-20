3e6783fe8ab9d2ea50bd0d9e01729d3f73431355
Anurag Pal <tanmaypal123@outlook.com>
2026-03-21 02:41:42 +0530

    implemented residual connections

--- bigram.py
+++ bigram.py
@@ -116,8 +116,8 @@ class Block(nn.Module):
         self.ffwd = FeedForward(n_embd) #computation is done here.
 
     def forward(self, x):
-        x = self.sa(x)
-        x = self.ffwd(x)
+        x = x + self.sa(x)
+        x = x + self.ffwd(x)
         return x
 
 # super simple bigram model
